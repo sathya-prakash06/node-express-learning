eb575aab36cbfdda2911ec7973307a5ee1c23591
Sathya <sathyaprakash.sp.6@gmail.com>
2022-05-09 13:46:22 +0530

    nodemon

---
+++ 01.node-tutorial/app.js
@@ -0,0 +1,7 @@
+const _ = require("lodash");
+
+const arr = [1, [1, 7, 3], [4, 5, 6], [7, 8, 9, 44]];
+
+console.log(_.flatten(arr));
+console.log('Hello World');
+console.log('Welcome to the jungle');
