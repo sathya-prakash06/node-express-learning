2555dbce6ac5d193dfdf8412d2054e6cffec4fc0
Sathya <sathyaprakash.sp.6@gmail.com>
2022-05-09 19:24:53 +0530

    http css js

--- 02.express-tutorial/finished/02.http-app.js
+++ 02.express-tutorial/finished/02.http-app.js
@@ -1,10 +1,7 @@
 const http = require("http");
 const { readFileSync } = require("fs");
 
-const homePage = readFileSync("./navbar-app/index.html");
-const homeStyles = readFileSync("./navbar-app/styles.css");
-const homeLogic = readFileSync("./navbar-app/browser-app.js");
-
+const homePage = readFileSync("./methods-public/index.html");
 const server = http.createServer((req, res) => {
   const url = req.url;
   if (url === "/") {
@@ -16,14 +13,6 @@ const server = http.createServer((req, res) => {
   else if (url === "/about") {
     res.writeHead(200, { "content-type": "text/plain" });
     res.write("<h1>..............About page..............</h1>");
-    res.end();
-  } else if (url === "/styles.css") {
-    res.writeHead(200, { "content-type": "text/css" });
-    res.write(homeStyles);
-    res.end();
-  } else if (url === "/browser-app.js") {
-    res.writeHead(200, { "content-type": "text/javascript" });
-    res.write(homeLogic);
     res.end();
   }
   // 404 page not found
